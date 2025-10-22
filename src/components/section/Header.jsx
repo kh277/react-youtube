2b775203981755485e55e9ddc131e7e6a17df759
kh277 <jkhcoding@naver.com>
2025-10-22 16:05:34 +0900

    header part complete

--- src/components/section/Header.jsx
+++ src/components/section/Header.jsx
@@ -1,9 +1,93 @@
 import React from 'react'
 
+import { CiBaseball } from "react-icons/ci";
+import { CiCoins1 } from "react-icons/ci";
+import { CiBoxes } from "react-icons/ci";
+import { CiBullhorn } from "react-icons/ci";
+import { CiCoffeeCup } from "react-icons/ci";
+import { CiDumbbell } from "react-icons/ci";
+import { CiFries } from "react-icons/ci";
+import { CiMoneyBill } from "react-icons/ci";
+
+import { AiFillGithub } from "react-icons/ai";
+import { AiOutlineCodepen } from "react-icons/ai";
+import { AiFillYoutube } from "react-icons/ai";
+import { AiOutlineInstagram } from "react-icons/ai";
+
 const Header = () => {
     return (
         <header id='header' role='banner'>
-            Header
+            <h1 className='header__logo'>
+                <a href='/'>
+                    <em aria-hidden='true'></em>
+                    <span>webs<br />youtube</span>
+                </a>
+            </h1>
+
+            <nav className='header__menu'>
+                <ul className='menu'>
+                    <li className='active'>
+                        <a href='/'>
+                            <CiBaseball /> 항목1
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/today'>
+                            <CiMoneyBill /> 항목2
+                        </a>
+                    </li>
+                    <li>
+                        <a href='/developer'>
+                            <CiCoins1 /> 항목3
+                        </a>
+                    </li>
+                </ul>
+                <ul className='keyword'>
+                    <li>
+                        <a href='/search/webstoryboy'>webstoryboy</a>
+                    </li>
+                    <li>
+                        <a href='/search/html'>HTML</a>
+                    </li>
+                    <li>
+                        <a href='/search/css'>CSS</a>
+                    </li>
+                    <li>
+                        <a href='/search/javascript'>JavaScript</a>
+                    </li>
+                    <li>
+                        <a href='/search/react.js'>React.js</a>
+                    </li>
+                    <li>
+                        <a href='/search/vue.js'>Vue.js</a>
+                    </li>
+                    <li>
+                        <a href='/search/next.js'>Next.js</a>
+                    </li>
+                    <li>
+                        <a href='/search/node.js'>Node.js</a>
+                    </li>
+                    <li>
+                        <a href='/search/sql'>SQL</a>
+                    </li>
+                    <li>
+                        <a href='/search/React Portfolio'>portfolio</a>
+                    </li>
+                    <li>
+                        <a href='/search/NewJeans'>music</a>
+                    </li>
+                </ul>
+            </nav>
+            
+            <div className='header__sns'>
+                <ul>
+                    <li>
+                        <a href='https://github.com/kh277' rel='noopener noreferrer'>
+                            <AiFillGithub />
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </header>
     )
 }
